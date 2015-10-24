7d964b0acaee6caadd0ad891ca45a4e69c3fa9b4
Roman Palmin <roman.palmin@gmail.com>
2015-10-24 21:34:13 +0300

    Обновлен хинтер

--- gulp/tasks/jshinter.js
+++ gulp/tasks/jshinter.js
@@ -9,9 +9,17 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var config = require('../config').js;
 var stylish = require('jshint-stylish');
+var hintOptions = {
+    esnext: true,
+    curly: true,
+    eqeqeq: true,
+    forin: true,
+    freeze: true,
+    maxparams: 3
+}
 
-gulp.task('jshint', function () {
+gulp.task('jshinter', function () {
     return gulp.src(config.src)
-        .pipe(jshint({esnext:true}))
+        .pipe(jshint(hintOptions))
         .pipe(jshint.reporter(stylish));
 });
